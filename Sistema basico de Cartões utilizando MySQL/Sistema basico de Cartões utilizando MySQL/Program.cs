507d145a6f3d50ea61ff9c5012ba03b86d751ce0
Ryan Vicente <147751342+devRyanVicente@users.noreply.github.com>
2024-02-04 18:13:31 -0300

    Essential Files to Program

---
+++ Sistema basico de Cartões utilizando MySQL/Sistema basico de Cartões utilizando MySQL/Program.cs
@@ -0,0 +1,18 @@
+namespace Sistema_basico_de_Cartões_utilizando_MySQL
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new ViewForm());
+        }
+    }
+}
